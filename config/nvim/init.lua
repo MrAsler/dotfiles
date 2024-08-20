48010354487b3e274d7b7cd9f7fb8be3db6bb41f
Valter Santos <valter.c.santos@protonmail.com>
2024-08-20 13:45:54 +1200

    Improve nvim settings

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -35,3 +35,30 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
+
+-- NvimTree settings
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+    --hidden_display = "all",
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
+
+
